7ac2f2ab6bbd7cb9e116cac45ab8007d162ab7d7
zcstanley-at-663253428838 <--unset>
2021-08-02 06:17:53 -0500

    adding meg and trapper spritesheets, using  basic movement and standing animations

--- src/index.js
+++ src/index.js
@@ -20,14 +20,17 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-var capguy;
+var meg;
+var trapper;
 var cursors;
 var background;
+var meg_last_direction;
+var trapper_last_direction;
 
 function preload() {
-    this.load.multiatlas('cityscene', 'assets/cityscene.json', 'assets');
+    this.load.multiatlas('meg_sprites', 'assets/meg_spritesheet.json', 'assets');
+    this.load.multiatlas('trapper_sprites', 'assets/trapper_spritesheet.json', 'assets');
     this.load.image('rocks', 'assets/free_rocks.png');
-    
 }
 
 function create() {
@@ -38,74 +41,247 @@ function create() {
     background.y = game.config.height/2;
     background.x = background.displayWidth*0.5;
 
-    capguy = this.physics.add.sprite(0, 400, 'cityscene', 'capguy/walk/001.png');
-    capguy.setScale(0.5, 0.5);
-    capguy.setBounce(0.2);
+    meg = this.physics.add.sprite(0, 400, 'meg_sprites', 'meg_sprite_21.png');
+    meg.setScale(1, 1);
+    meg.setBounce(0.2);
+    trapper = this.physics.add.sprite(400, 400, 'trapper_sprites', 'trapper_sprite_78.png');
+    trapper.setScale(1.2, 1.2);
+    trapper.setBounce(0.2);
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    var walkFrameNames = this.anims.generateFrameNames('cityscene', {
-        start: 1, end: 8, zeroPad: 4, prefix: 'capguy/walk/', suffix: '.png'
+    var megWalkUpFrameNames = this.anims.generateFrameNames('meg_sprites', {
+        start: 3, end: 10, prefix: 'meg_sprite_', suffix: '.png'
     });
-
-    var standFrameNames = this.anims.generateFrameNames('cityscene', {
-        start: 1, end: 1, zeroPad: 4, prefix: 'capguy/walk/', suffix: '.png'
+    var megWalkDownFrameNames = this.anims.generateFrameNames('meg_sprites', {
+        start: 22, end: 29, prefix: 'meg_sprite_', suffix: '.png'
     });
+    var megWalkLeftFrameNames = this.anims.generateFrameNames('meg_sprites', {
+        start: 11, end: 20, prefix: 'meg_sprite_', suffix: '.png'
+    });
+    var megStandUpFrameNames = this.anims.generateFrameNames('meg_sprites', {
+        start: 2, end: 2, prefix: 'meg_sprite_', suffix: '.png'
+    });
+    var megStandDownFrameNames = this.anims.generateFrameNames('meg_sprites', {
+        start: 21, end: 21, prefix: 'meg_sprite_', suffix: '.png'
+    });
+    var megStandLeftFrameNames = this.anims.generateFrameNames('meg_sprites', {
+        start: 11, end: 11, prefix: 'meg_sprite_', suffix: '.png'
+    });
+    var trapperWalkUpFrameNames = this.anims.generateFrameNames('trapper_sprites', {
+        start: 61, end: 68, prefix: 'trapper_sprite_', suffix: '.png'
+    });
+    var trapperWalkDownFrameNames = this.anims.generateFrameNames('trapper_sprites', {
+        start: 79, end: 86, prefix: 'trapper_sprite_', suffix: '.png'
+    });
+    var trapperWalkLeftFrameNames = this.anims.generateFrameNames('trapper_sprites', {
+        start: 69, end: 77, prefix: 'trapper_sprite_', suffix: '.png'
+    });
+    var trapperStandUpFrameNames = this.anims.generateFrameNames('trapper_sprites', {
+        start: 61, end: 61, prefix: 'trapper_sprite_', suffix: '.png'
+    });
+    var trapperStandDownFrameNames = this.anims.generateFrameNames('trapper_sprites', {
+        start: 79, end: 79, prefix: 'trapper_sprite_', suffix: '.png'
+    });
+    var trapperStandLeftFrameNames = this.anims.generateFrameNames('trapper_sprites', {
+        start: 69, end: 69, prefix: 'trapper_sprite_', suffix: '.png'
+    }); 
 
     this.anims.create({
-        key: 'walk',
-        frames: walkFrameNames, 
-        frameRate: 10, 
+        key: 'meg-walk-up',
+        frames: megWalkUpFrameNames,
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'meg-walk-down',
+        frames: megWalkDownFrameNames,
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'meg-walk-left',
+        frames: megWalkLeftFrameNames,
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'meg-stand-left',
+        frames: megStandLeftFrameNames,
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'meg-stand-up',
+        frames: megStandUpFrameNames,
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'meg-stand-down',
+        frames: megStandDownFrameNames,
+        frameRate: 10,
         repeat: -1
     });
 
     this.anims.create({
-        key: 'jump',
-        frames: standFrameNames, 
-        frameRate: 10, 
+        key: 'trapper-walk-up',
+        frames: trapperWalkUpFrameNames,
+        frameRate: 10,
         repeat: -1
     });
-
     this.anims.create({
-        key: 'stand-still', 
-        frames: standFrameNames, 
-        frameRate: 10, 
+        key: 'trapper-walk-down',
+        frames: trapperWalkDownFrameNames,
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'trapper-walk-left',
+        frames: trapperWalkLeftFrameNames,
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'trapper-stand-left',
+        frames: trapperStandLeftFrameNames,
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'trapper-stand-up',
+        frames: trapperStandUpFrameNames,
+        frameRate: 10,
+        repeat: -1
+    });
+    this.anims.create({
+        key: 'trapper-stand-down',
+        frames: trapperStandDownFrameNames,
+        frameRate: 10,
         repeat: -1
     });
 }
 
 function update(time, delta) {
-    if(capguy.x > 850) {
-        capguy.x = -50;
+    if(meg.x > 850) {
+        meg.x = -50;
     }
-    else if(capguy.x < -50) {
-        capguy.x = 850;
+    else if(meg.x < -50) {
+        meg.x = 850;
+    }
+    if(meg.y > 650) {
+        meg.y = -50;
+    }
+    else if(meg.y < -50) {
+        meg.y = 650;
     }
 
     if(cursors.right.isDown) {
-        capguy.setVelocityX(300);
-        capguy.flipX = false;
-        capguy.anims.play('walk', true);
+        meg.setVelocityX(200);
+        meg.setVelocityY(0);
+        meg.flipX = true;
+        meg.anims.play('meg-walk-left', true);
+        meg_last_direction = "right";
     }
     else if(cursors.left.isDown) {
-        capguy.setVelocityX(-300);
-        capguy.flipX = true;
-        capguy.anims.play('walk', true);
+        meg.setVelocityX(-200);
+        meg.setVelocityY(0);
+        meg.flipX = false;
+        meg.anims.play('meg-walk-left', true);
+        meg_last_direction = "left";
     }
-    else {
-        capguy.setVelocityX(0);
-        capguy.anims.play('stand-still', true);
-    }
-
-    if(cursors.up.isDown) {
-        capguy.setVelocityY(-300);
-        // capguy.anims.play('jump', true);
+    else if(cursors.up.isDown) {
+        meg.setVelocityX(0);
+        meg.setVelocityY(-200);
+        meg.anims.play('meg-walk-up', true);
+        meg_last_direction = "up";
     }
     else if(cursors.down.isDown) {
-        capguy.setVelocityY(300);
-        // capguy.anims.play('jump', true);
+        meg.setVelocityX(0);
+        meg.setVelocityY(200);
+        meg.anims.play('meg-walk-down', true);
+        meg_last_direction = "down";
     }
     else {
-        capguy.setVelocityY(0);
+        meg.setVelocityX(0);
+        meg.setVelocityY(0);
+        if(meg_last_direction === "right") {
+            meg.flipX = true;
+            meg.anims.play('meg-stand-left', true);
+        }
+        else if(meg_last_direction === "left") {
+            meg.anims.play('meg-stand-left', true);
+        }
+        else if(meg_last_direction === "up") {
+            meg.anims.play('meg-stand-up', true);
+        }
+        else if(meg_last_direction === "down") {
+            meg.anims.play('meg-stand-down', true);
+        }
+    }
+
+    if (Phaser.Math.Distance.BetweenPoints(meg, trapper) < 80000) {
+        if(meg.x - trapper.x >= Math.abs(meg.y - trapper.y) + 20) {
+            trapper.setVelocityX(100);
+            trapper.setVelocityY(0);
+            trapper.flipX = true;
+            trapper.anims.play('trapper-walk-left', true);
+            trapper_last_direction = "right";
+        }
+        else if(meg.x - trapper.x <= -Math.abs(meg.y - trapper.y) - 20) {
+            trapper.setVelocityX(-100);
+            trapper.setVelocityY(0);
+            trapper.flipX = false;
+            trapper.anims.play('trapper-walk-left', true);
+            trapper_last_direction = "left";
+        }
+        else if(meg.y - trapper.y <= -Math.abs(meg.x - trapper.x) - 20) {
+            trapper.setVelocityX(0);
+            trapper.setVelocityY(-100);
+            trapper.anims.play('trapper-walk-up', true);
+            trapper_last_direction = "up";
+        }
+        else if(meg.y - trapper.y >= Math.abs(meg.x - trapper.x) + 20) {
+            trapper.setVelocityX(0);
+            trapper.setVelocityY(100);
+            trapper.anims.play('trapper-walk-down', true);
+            trapper_last_direction = "down";
+        }
+        else if(Math.abs(meg.y - trapper.y) <= 5) {
+            trapper.setVelocityX(0);
+            trapper.setVelocityY(0);
+            if(trapper_last_direction === "up") {
+                trapper.anims.play('trapper-stand-up', true);
+            }
+            else if(trapper_last_direction === "down") {
+                trapper.anims.play('trapper-stand-down', true);
+            }
+            else if(trapper_last_direction === "right") {
+                trapper.flipX = true;
+                trapper.anims.play('trapper-stand-left', true);
+            }
+            else if(trapper_last_direction === "left") {
+                trapper.flipX = false;
+                trapper.anims.play('trapper-stand-left', true);
+            }
+        }
+        else if(Math.abs(meg.x - trapper.x) <= 5) {
+            trapper.setVelocityX(0);
+            trapper.setVelocityY(0);
+            if(trapper_last_direction === "right") {
+                trapper.flipX = true;
+                trapper.anims.play('trapper-stand-left', true);
+            }
+            else if(trapper_last_direction === "left") {
+                trapper.flipX = false;
+                trapper.anims.play('trapper-stand-left', true);
+            }
+            else if(trapper_last_direction === "up") {
+                trapper.anims.play('trapper-stand-up', true);
+            }
+            else if(trapper_last_direction === "down") {
+                trapper.anims.play('trapper-stand-down', true);
+            }
+        }
     }
 }
